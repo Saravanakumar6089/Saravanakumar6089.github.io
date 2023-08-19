5dc8c2932ea4323d3c684f7bb8ed8b85711e68d6
Saravanakumar6089 <138742119+Saravanakumar6089@users.noreply.github.com>
2023-08-19 14:15:58 +0530

    Delete Website directory

--- Website/sample.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-$_SESSION["name"]="ram";
-session_destroy();
-echo $_SESSION["name"];
-?>
--- Website/script.js
+++
@@ -1,7 +0,0 @@
-function droplist(){
-    if (document.getElementById("d4").style.display==="none"){
-        document.getElementById("d4").style.display="inline-block";} 
-
-    else if (document.getElementById("d4").style.display!=="none"){
-        document.getElementById("d4").style.display="none";}
-}
